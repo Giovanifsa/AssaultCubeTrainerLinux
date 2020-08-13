1ccf9778bc603304a99f28181888008b09ef76db
Giovanifsa <giovanifsa@gmail.com>
2020-08-13 16:39:01 -0300

    Definições do projeto

---
+++ Library/librarymain.cpp
@@ -0,0 +1,5 @@
+#include "librarymain.h"
+
+LibraryMain::LibraryMain()
+{
+}
---
+++ Library/librarymain.h
@@ -0,0 +1,12 @@
+#ifndef LIBRARYMAIN_H
+#define LIBRARYMAIN_H
+
+#include "Library_global.h"
+
+class LIBRARY_EXPORT LibraryMain
+{
+public:
+    LibraryMain();
+};
+
+#endif // LIBRARYMAIN_H
